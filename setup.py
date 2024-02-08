a6d34e59c0562f1eb49b990da111c109d4c60148
Chizz98 <116356082+Chizz98@users.noreply.github.com>
2024-02-08 13:09:42 +0100

    Added extra files

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name="lettuceSee",
     url="https://github.com/Chizz98/LettuceSee",
     packages=find_packages(),
+    author="Chris Dijkstra",
+    author_email="chris_dijkstra98@hotmail.com",
     version=__version__,
     install_requires=[
         "scikit-image>=0.10.0"
