390d7e1c37b8e256046171870feb1a0387a8701a
Chizz98 <116356082+Chizz98@users.noreply.github.com>
2024-02-08 10:42:59 +0100

    test

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='lettuceSee',
     packages=['lettuceSee'],
-    version='v0.0.1a0',
+    version='0.0.1a0',
     description='Test build',
     author='',
     author_email='',
     url='https://github.com/Chizz98/LettuceSee',
     download_url='https://github.com/Chizz98/LettuceSee/archive/refs/tags/v0.0.1-alpha.tar.gz',
     keywords=['tag1', 'tag2'],
-    classifiers=[],
+    classifiers=["test"],
 )
