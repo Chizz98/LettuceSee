c980d1909f417399343ad705401f6d6999afe4d5
Chizz98 <116356082+Chizz98@users.noreply.github.com>
2024-02-08 10:23:19 +0100

    Build base

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='lettuceSee',
+    packages=['lettuceSee'],
+    version='v0.0.1-alpha',  # Ideally should be same as your GitHub release tag varsion
+    description='Test build',
+    author='',
+    author_email='',
+    url='https://github.com/Chizz98/LettuceSee',
+    download_url='https://github.com/Chizz98/LettuceSee/archive/refs/tags/v0.0.1-alpha.tar.gz',
+    keywords=['tag1', 'tag2'],
+    classifiers=[],
+)
