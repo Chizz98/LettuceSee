f71bda779abb1f4b2e0549499fe5ceebd4362e76
Dijkstra <116356082+Chizz98@users.noreply.github.com>
2024-11-14 12:10:51 +0100

    Fixed version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="A package of image analysis algorithms suited for plants",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.1.0",
+    version="0.1.2",
     install_requires=[
         "scikit-image",
         "scipy",
