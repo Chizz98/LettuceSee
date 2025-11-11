7628b755d90939f02d24b8521729b71cd40a090d
Chizz98 <116356082+Chizz98@users.noreply.github.com>
2025-11-11 12:59:12 +0100

    Fixed version in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="A package of image analysis algorithms suited for plants",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.2.1",
+    version="0.2.2",
     install_requires=[
         "scikit-image",
         "scipy",
